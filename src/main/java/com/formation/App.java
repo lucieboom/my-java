2ee1601d8932b73580d31e2cd3ec5bfbaaf4cdba
unknown <yeung.lucie@gmail.com>
2020-08-07 10:05:15 +0200

    Maven

--- src/main/java/com/formation/App.java
+++ src/main/java/com/formation/App.java
@@ -9,6 +9,8 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class App {
 	public static void main(String[] args) {
 		
+		System.out.println("Hello Lulu!");
+		
 		if (args.length < 1) {
 			System.out.println("Please provide an input");
 			System.exit(0);
